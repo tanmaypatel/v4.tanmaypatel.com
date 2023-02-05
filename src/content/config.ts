da0c25416fd1525b09e27525fe03e162e72be69c
Tanmay Patel <me@tanmaypatel.com>
2023-02-05 15:25:24 +0400

    feat: Migrated initial content from v3 without styling

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import { defineCollection, z } from "astro:content";
+
+const aboutMeSchema = z.object({
+  about: z.string(),
+  thingsILove: z.string().array(),
+});
+
+const aboutMe = defineCollection({
+  schema: aboutMeSchema,
+});
+
+const roleSchema = z.object({
+  role: z.string(),
+  from: z.string(),
+  to: z.string(),
+  skills: z.string().array(),
+  responsibilities: z.string().array(),
+});
+
+const experienceSchema = z.object({
+  compnay: z.string(),
+  roles: z.array(roleSchema),
+});
+
+const experience = defineCollection({
+  schema: experienceSchema,
+});
+
+export const collections = { experience };
