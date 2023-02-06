5fc009ef1831f84ce676d7e920666299e68b8c66
Tanmay Patel <me@tanmaypatel.com>
2023-02-06 07:21:11 +0400

    feat: setup the base and layout for styling using tailwind and SASS in the website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+        serif: ["Roboto Slab", ...defaultTheme.fontFamily.serif],
+        mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
