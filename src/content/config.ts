0f36f449d65e535ffea07f1ed03fbb38431d40f0
Tanmay Patel <me@tanmaypatel.com>
2023-02-09 08:02:43 +0400

    chore: Fixed code formatting using prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,29 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const aboutMeSchema = z.object({
-  about: z.string(),
-  thingsILove: z.string().array(),
+    about: z.string(),
+    thingsILove: z.string().array()
 });
 
 const aboutMe = defineCollection({
-  schema: aboutMeSchema,
+    schema: aboutMeSchema
 });
 
 const roleSchema = z.object({
-  role: z.string(),
-  from: z.string(),
-  to: z.string(),
-  skills: z.string().array(),
-  responsibilities: z.string().array(),
+    role: z.string(),
+    from: z.string(),
+    to: z.string(),
+    skills: z.string().array(),
+    responsibilities: z.string().array()
 });
 
 const experienceSchema = z.object({
-  compnay: z.string(),
-  roles: z.array(roleSchema),
+    compnay: z.string(),
+    roles: z.array(roleSchema)
 });
 
 const experience = defineCollection({
-  schema: experienceSchema,
+    schema: experienceSchema
 });
 
 export const collections = { experience };
