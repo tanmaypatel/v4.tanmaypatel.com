836ba510e0a52d8ea60849d2156f648b6bab1ef2
Tanmay Patel <tanmay.patel@axiomtelecom.com>
2023-02-09 12:36:51 +0400

    feat: Added responsiveness for Intro Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap"
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tanmaypatel.com",
   integrations: [mdx(), sitemap(), tailwind({
-    config: { applyBaseStyles: false },
-  })],
+    config: {
+      applyBaseStyles: false
+    }
+  }), react()]
 });
